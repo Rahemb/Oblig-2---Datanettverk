16cfb495b925c891ac6d69b63b96d8966a7a950f
Rahemb <143782157+Rahemb@users.noreply.github.com>
2025-04-03 17:14:59 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,23 +1,26 @@
-from socket import *
 import argparse
+from socket import *
 
-# Parser for kommandolinjeargumenter
-parser = argparse.ArgumentParser(description='Simple HTTP Client')
-parser.add_argument('-i', '--ip', required=True, help='Server IP address')
-parser.add_argument('-p', '--port', type=int, required=True, help='Server port number')
-parser.add_argument('-f', '--file', required=True, help='File to request (e.g., index.html)')
+# Parse command-line arguments
+parser = argparse.ArgumentParser(description="Simple HTTP Client")
+parser.add_argument("-i", "--ip", required=True, help="Server IP address")
+parser.add_argument("-p", "--port", type=int, required=True, help="Server port")
+parser.add_argument("-f", "--file", required=True, help="File to request from the server")
 args = parser.parse_args()
 
-# Opprette TCP socket
+# Create a TCP socket
 clientSocket = socket(AF_INET, SOCK_STREAM)
+
+# Connect to the server using the provided IP and port
 clientSocket.connect((args.ip, args.port))
 
-# Lage og sende HTTP GET-forespørsel
+# Send an HTTP GET request for the specified file
 request = f"GET /{args.file} HTTP/1.1\r\nHost: {args.ip}\r\n\r\n"
 clientSocket.send(request.encode())
 
-# Motta og vise respons
+# Receive and print the response from the server
 response = clientSocket.recv(4096)
 print("Server response:\n", response.decode())
 
+# Close the connection
 clientSocket.close()
